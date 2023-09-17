8eb3070703b2bbc3c8b5ea8fc30782462c44adba
gspro <gavin.sproles@gmail.com>
2023-09-17 17:34:20 -0400

    add culture filter view

--- screens/Culture/CultureScreen.tsx
+++ screens/Culture/CultureScreen.tsx
@@ -1,6 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./cultureStyles";
-import { FlatList, Image, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  LayoutAnimation,
+  Pressable,
+  Text,
+  View,
+} from "react-native";
 import { FloatingActionButton } from "../../components";
 import Fontisto from "@expo/vector-icons/Fontisto";
 import GlobalStyles from "../../constants/GlobalStyles";
@@ -19,7 +27,7 @@ import {
   SupaBaseCultureType,
 } from "../../@types";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, Ionicons } from "@expo/vector-icons";
 import { useCultureContext } from "../../providers";
 
 type CultureScreenScreenNavigationProp = StackNavigationProp<
@@ -59,6 +67,8 @@ const calculateDaysPassed = (inoculation_date: string): string => {
 };
 
 export const CultureScreen = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
+
   const { cultures, setCultures } = useCultureContext();
   const { navigate } = useNavigation<CultureScreenScreenNavigationProp>();
 
@@ -90,92 +100,144 @@ export const CultureScreen = () => {
     }
   };
 
+  const handleToggleFilter = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setIsFilterOpen(!isFilterOpen);
+  };
+
   return (
     <View style={styles.wrap}>
-      <View
-        style={{
-          borderColor: "blue",
-          borderWidth: 1,
-          height: 50,
-          // marginBottom: 16,
-        }}
-      >
-        {/*// todo  */}
-      </View>
-      {cultures?.length === 0 ? (
-        <View style={styles.noCulturesWrap}>
-          <Image
-            source={require("../../assets/no_culture.png")}
-            resizeMode="contain"
-            style={styles.noCulturesImage}
-          />
-          <Text style={styles.noCulturesText}>
-            Oh No! You don't have any cultures yet!
-          </Text>
-          <Text style={styles.noCulturesText}>Tap here to add a culture</Text>
-          <Image
-            source={require("../../assets/bent_arrow.png")}
-            resizeMode="contain"
-            style={styles.noCulturesArrow}
+      {cultures === undefined ? (
+        <View style={styles.spinnerWrap}>
+          <ActivityIndicator
+            size={100}
+            color={GlobalStyles.colorSet.primaryColor}
           />
         </View>
       ) : (
-        <FlatList
-          data={cultures}
-          style={styles.flatList}
-          keyExtractor={(item: CultureType) => item.uuid}
-          renderItem={({ item, index }) => (
-            <View
-              key={item.uuid}
-              style={{
-                gap: 4,
-                marginTop: index === 0 ? 16 : undefined,
-                marginBottom: index === cultures!.length - 1 ? 96 : undefined,
-              }}
-            >
-              <View>
-                <Text style={{ textAlign: "right" }}>{item?.culture_id}</Text>
-              </View>
-              <View style={styles.itemWrap}>
-                {substrateIcon(item.substrate.type)}
-                <View style={styles.nameWrap}>
-                  <Text style={{ fontSize: 20 }}>{item.culture_name}</Text>
-                  <View style={styles.scientificNameWrap}>
-                    <Text style={styles.scientificNameText}>
-                      {item.scientificName.genus}
-                    </Text>
-                    <Text style={styles.scientificNameText}>
-                      {item.scientificName.species}
-                    </Text>
-                  </View>
-                </View>
-                <View style={styles.detailsWrap}>
-                  {statusBadge(item.status ?? "Inoculated")}
-                  <Text style={styles.daysText}>
-                    {calculateDaysPassed(item.date_inoculated)} Days Old
-                  </Text>
-                </View>
-                <Entypo
-                  name="lab-flask"
-                  size={24}
-                  color="gray"
-                  style={styles.icon}
-                />
-              </View>
+        <>
+          {cultures?.length === 0 ? (
+            <View style={styles.noCulturesWrap}>
+              <Image
+                source={require("../../assets/no_culture.png")}
+                resizeMode="contain"
+                style={styles.noCulturesImage}
+              />
+              <Text style={styles.noCulturesText}>
+                Oh No! You don't have any cultures yet!
+              </Text>
+              <Text style={styles.noCulturesText}>
+                Tap here to add a culture
+              </Text>
+              <Image
+                source={require("../../assets/bent_arrow.png")}
+                resizeMode="contain"
+                style={styles.noCulturesArrow}
+              />
             </View>
+          ) : (
+            <>
+              <View
+                style={{
+                  ...styles.filterHeader,
+                  height: isFilterOpen ? "95%" : "auto",
+                }}
+              >
+                <View
+                  style={{
+                    ...styles.filterWrap,
+                    flex: isFilterOpen ? 1 : undefined,
+                  }}
+                >
+                  <Pressable
+                    style={styles.filterButton}
+                    onPress={handleToggleFilter}
+                  >
+                    <Ionicons
+                      style={{ marginLeft: "auto" }}
+                      name="filter"
+                      size={28}
+                      color="white"
+                    />
+                    <Text style={styles.filterText}>Filters</Text>
+                  </Pressable>
+                </View>
+              </View>
+              <View style={styles.bodyWrap}>
+                {isFilterOpen ? (
+                  <View style={styles.filterViewBody}>
+                    <Text>Showing {cultures?.length} Cultures</Text>
+                  </View>
+                ) : (
+                  <FlatList
+                    data={cultures}
+                    style={styles.flatList}
+                    keyExtractor={(item: CultureType) => item.uuid}
+                    renderItem={({ item, index }) => (
+                      <View
+                        key={item.uuid}
+                        style={{
+                          gap: 4,
+                          marginTop: index === 0 ? 16 : undefined,
+                          marginBottom:
+                            index === cultures!.length - 1 ? 96 : undefined,
+                        }}
+                      >
+                        <View>
+                          <Text style={{ textAlign: "right" }}>
+                            {item?.culture_id}
+                          </Text>
+                        </View>
+                        <View style={styles.itemWrap}>
+                          {substrateIcon(item.substrate.type)}
+                          <View style={styles.nameWrap}>
+                            <Text style={{ fontSize: 20 }}>
+                              {item.culture_name}
+                            </Text>
+                            <View style={styles.scientificNameWrap}>
+                              <Text style={styles.scientificNameText}>
+                                {item.scientificName.genus}
+                              </Text>
+                              <Text style={styles.scientificNameText}>
+                                {item.scientificName.species}
+                              </Text>
+                            </View>
+                          </View>
+                          <View style={styles.detailsWrap}>
+                            {statusBadge(item.status ?? "Inoculated")}
+                            <Text style={styles.daysText}>
+                              {calculateDaysPassed(item.date_inoculated)} Days
+                              Old
+                            </Text>
+                          </View>
+                          <Entypo
+                            name="lab-flask"
+                            size={24}
+                            color="gray"
+                            style={styles.icon}
+                          />
+                        </View>
+                      </View>
+                    )}
+                  />
+                )}
+              </View>
+            </>
           )}
-        />
+          {!isFilterOpen && (
+            <FloatingActionButton
+              onPress={() => navigate("AddCulture")}
+              icon={
+                <Fontisto
+                  name="injection-syringe"
+                  size={26}
+                  color={`${GlobalStyles.colorSet.accentColor}`}
+                />
+              }
+            />
+          )}
+        </>
       )}
-      <FloatingActionButton
-        onPress={() => navigate("AddCulture")}
-        icon={
-          <Fontisto
-            name="injection-syringe"
-            size={26}
-            color={`${GlobalStyles.colorSet.accentColor}`}
-          />
-        }
-      />
     </View>
   );
 };
--- screens/Culture/cultureStyles.ts
+++ screens/Culture/cultureStyles.ts
@@ -4,12 +4,39 @@ import GlobalStyles from "../../constants/GlobalStyles";
 const styles = StyleSheet.create({
   wrap: {
     flex: 1,
+    backgroundColor: GlobalStyles.colorSet.primaryColor,
+  },
+  spinnerWrap: {
+    flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  filterHeader: {
+    backgroundColor: GlobalStyles.colorSet.primaryColor,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  filterWrap: {
     marginTop: StatusBar.currentHeight,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "flex-end",
+  },
+  filterButton: {
+    flexDirection: "row",
+    gap: 6,
+  },
+  filterText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "500",
+    marginTop: 2,
   },
   noCulturesWrap: {
     flex: 1,
-    marginTop: "53%",
+    marginTop: "78%",
     alignItems: "center",
     gap: 5,
   },
@@ -32,9 +59,18 @@ const styles = StyleSheet.create({
     borderRadius: 16,
   },
   statusBadgeText: { color: "white", fontSize: 12, textAlign: "center" },
-  flatList: {
-    height: "100%",
+  bodyWrap: {
     flex: 1,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    backgroundColor: "#fff",
+  },
+  filterViewBody: {
+    flex: 1,
+    marginLeft: 16,
+    justifyContent: "center",
+  },
+  flatList: {
     ...GlobalStyles.defaultPageStyles,
   },
   itemWrap: {
